cfd0576272338345d29a9e71921bb3b66c618224
vishaalupot <vishaalsupotofficial@gmail.com>
2024-08-05 16:48:52 +0400

    Added Partner

---
+++ src/components/Partner/Partners.js
@@ -0,0 +1,293 @@
+import React from 'react'
+import './Partner.css';
+import AT from '../../assets/AT&T.svg'
+import first from '../../assets/first.jpg'
+import second from '../../assets/second.jpg'
+import third from '../../assets/third.jpg'
+import fourth from '../../assets/fourth.jpg'
+import fifth from '../../assets/fifth.jpg'
+import sixth from '../../assets/sixth.jpg'
+import seventh from '../../assets/seventh.jpg'
+import eight from '../../assets/eight.jpg'
+import nine from '../../assets/nine.jpg'
+import ten from '../../assets/ten.png'
+import eleven from '../../assets/eleven.jpg'
+import twelve from '../../assets/twelve.jpg'
+import thirteen from '../../assets/thirteen.jpg'
+import fourteen from '../../assets/fourteen.jpg'
+import fifteen from '../../assets/fifteen.jpg'
+import sixteen from '../../assets/sixteen.jpg'
+import seventeen from '../../assets/seventeen.jpg'
+import eighteen from '../../assets/eighteen.jpg'
+import p1 from '../../assets/p1.png'
+import p2 from '../../assets/p2.jpg'
+import p3 from '../../assets/p3.jpg'
+import p4 from '../../assets/p4.jpg'
+import p5 from '../../assets/p5.jpeg'
+import p6 from '../../assets/p6.jpeg'
+import p7 from '../../assets/p7.jpeg'
+import p8 from '../../assets/p8.jpg'
+import p9 from '../../assets/p9.jpg'
+import p10 from '../../assets/p10.jpg'
+import p11 from '../../assets/p11.png'
+import p12 from '../../assets/p12.jpg'
+import p13 from '../../assets/p13.jpg'
+import p14 from '../../assets/p14.jpg'
+import p15 from '../../assets/p15.jpg'
+import p16 from '../../assets/p16.jpg'
+const Partners = () => {
+    return (
+        <div className='flex justify-center equal-padding'>
+         <div className='heading1'>Trusted by</div>
+         <div>
+            <img src={AT} className='partnerImages'/>
+            <img src={AT} className='partnerImages'/>
+            <img src={AT} className='partnerImages'/>
+            <img src={AT} className='partnerImages'/>
+            <img src={AT} className='partnerImages'/>
+            <img src={AT} className='partnerImages'/>
+         </div>
+        
+
+         <div className='heading1'>Our Recent Works</div>
+
+         <div className='cards-container'>
+            <div className='cards'>
+                <img src={first} className='card-image'/>
+                <div className='cards-texts'>
+                    <div className='cards-heading'>
+                        Western Midstream
+                    </div>
+                    <div className='cards-para'>
+                    Houston, TX Western Midstream Partners in The Woodlands, Texas, remodeled their executiveboardroom, training rooms, CEO office, and CFO office using the latest AV technology. …
+                    </div>
+
+                </div>
+            </div>
+            <div className='cards'>
+                <img src={second} className='card-image'/>
+                <div className='cards-texts'>
+                    <div className='cards-heading'>
+                    Indeed
+                    </div>
+                    <div className='cards-para'>
+                    Multiple Global Locations With over 13,000 global employees working in over forty-five countries, Indeed uses extensive AV technology to impact the workplace community. Features … 
+                    </div>
+
+                </div>
+            </div>
+            <div className='cards'>
+                <img src={third} className='card-image'/>
+                <div className='cards-texts'>
+                    <div className='cards-heading'>
+                    Apple Park
+                    </div>
+                    <div className='cards-para'>
+                    Apple’s corporate headquarters is a 2.8 million SF complex that hosts nearly 12,000 employees and visitors on a daily basis. Ford provided AV solutions … Read more
+                    </div>
+
+                </div>
+            </div>
+            <div className='cards'>
+                <img src={fourth} className='card-image'/>
+                <div className='cards-texts'>
+                    <div className='cards-heading'>
+                    PGA Headquarters
+                    </div>
+                    <div className='cards-para'>
+                    Dallas, TX The PGA of America moved its headquarters from Florida to North Dallas. The new 4-story headquarters building is on a 660-acre campus … Read more
+                    </div>
+
+                </div>
+            </div>
+            <div className='cards'>
+                <img src={fifth} className='card-image'/>
+                <div className='cards-texts'>
+                    <div className='cards-heading'>
+                    William P. Hobby Airport
+                    </div>
+                    <div className='cards-para'>
+                    Houston, TX Western Midstream Partners in The Woodlands, Texas, remodeled their executiveboardroom, training rooms, CEO office, and CFO office using the latest AV technology. …
+                    </div>
+
+                </div>
+            </div>
+            <div className='cards'>
+                <img src={sixth} className='card-image'/>
+                <div className='cards-texts'>
+                    <div className='cards-heading'>
+                    Crossroads Christian Church
+                    </div>
+                    <div className='cards-para'>
+                    Grand Prairie, TX With a complete overhaul to both the audio and video systems, Crossroads Christian Church now houses a variety of the latest … Read more
+                    </div>
+
+                </div>
+            </div>
+            <div className='cards'>
+                <img src={seventh} className='card-image'/>
+                <div className='cards-texts'>
+                    <div className='cards-heading'>
+                    Liberty Mutual
+                    <div className='cards-para'>
+                    Denver, CO Liberty Mutual Group is a global insurer and Fortune 100 company, with technological demand of 24/7 operations. Liberty Global constructed a new … Read more
+                    </div>
+                    </div>
+
+                </div>
+            </div>
+            <div className='cards'>
+                <img src={eight} className='card-image'/>
+                <div className='cards-texts'>
+                    <div className='cards-heading'>
+                    Chase Oak Church
+                    </div>
+                    <div className='cards-para'>
+                    Sachse, TX The Woodbridge Campus is the fourth campus of Chase Oak Church. To achieve the desired contemporary worship service, the facility’s system is … Read more
+                    </div>
+
+                </div>
+            </div>
+            <div className='cards'>
+                <img src={nine} className='card-image'/>
+                <div className='cards-texts'>
+                    <div className='cards-heading'>
+                        Western Midstream
+                    </div>
+                    <div className='cards-para'>
+                    Houston, TX Western Midstream Partners in The Woodlands, Texas, remodeled their executiveboardroom, training rooms, CEO office, and CFO office using the latest AV technology. …
+                    </div>
+
+                </div>
+            </div>
+         </div>
+
+         
+         <div className='heading1'>Latest Solutions</div>
+
+        <div className='cards-container'>
+        <div className='cards'>
+            <img src={ten} className='card-image'/>
+            <div className='cards-texts'>
+                <div className='cards-heading'>
+                Intelligent Meetings with Microsoft Teams Rooms: Insights from Our Recent Webinar
+                </div>
+                <div className='cards-para'>
+                In our latest webinar, we delved into how AI is transforming workplace meetings, focusing specifically on Microsoft Teams Rooms (MTRs). Below is a summary … Read more
+                </div>
+
+            </div>
+        </div>
+        <div className='cards'>
+            <img src={eleven} className='card-image'/>
+            <div className='cards-texts'>
+                <div className='cards-heading'>
+                Experience Extraordinary with Ford AV
+                </div>
+                <div className='cards-para'>
+                At Ford, we are driven by more than just technology. We are a team of extraordinary people who take immense pride in crafting exceptional … Read more
+                </div>
+
+            </div>
+        </div>
+        <div className='cards'>
+            <img src={twelve} className='card-image'/>
+            <div className='cards-texts'>
+                <div className='cards-heading'>
+                Innovation in Medical Simulation
+                <div className='cards-para'>
+                Imagine practicing a complex surgical procedure on a high-fidelity mannequin before ever setting foot in a real operating room. Or navigating a critical care … Read more                </div>
+                </div>
+
+            </div>
+        </div>
+        <div className='cards'>
+            <img src={thirteen} className='card-image'/>
+            <div className='cards-texts'>
+                <div className='cards-heading'>
+                Microsoft Teams Rooms: Connect Across the Hybrid Workplace
+                <div className='cards-para'>
+                A Microsoft Teams Rooms solution seamlessly integrated into the conference room ensures that all participants are visible, audible, and actively engaged from any location. … Read more                </div>
+                </div>
+
+            </div>
+        </div>
+        <div className='cards'>
+            <img src={fourteen} className='card-image'/>
+            <div className='cards-texts'>
+                <div className='cards-heading'>
+                Crestron AirMedia
+                <div className='cards-para'>
+                Simplify Today. Amplify Tomorrow. Crestron AirMedia® technology is made for growing companies, providing a fast and simple wireless meeting solution from the start, while … Read more                </div>
+                </div>
+
+            </div>
+        </div>
+        <div className='cards'>
+            <img src={fifteen} className='card-image'/>
+            <div className='cards-texts'>
+                <div className='cards-heading'>
+                Crestron Flex Room Solutions
+                <div className='cards-para'>
+                Crestron Flex Room Solutions are powerful meeting room solutions that empower hybrid teams. Whatever the size of the organization, Crestron offers a solution that … Read more                </div>
+                </div>
+
+            </div>
+        </div>
+        <div className='cards'>
+            <img src={sixteen} className='card-image'/>
+            <div className='cards-texts'>
+                <div className='cards-heading'>
+                Microsoft Copilot
+                <div className='cards-para'>
+                Unlocking the Power of Productivity: Microsoft Copilot for Your Organization  The corporate landscape is changing. Workplaces demand agility, efficiency, and innovation more than ever. … Read more                </div>
+                </div>
+                </div>
+        </div>
+        <div className='cards'>
+            <img src={seventeen} className='card-image'/>
+            <div className='cards-texts'>
+                <div className='cards-heading'>
+                Technology Solutions for Government
+                <div className='cards-para'>
+                Government meeting rooms across the U.S. continually seek ways to improve their collaboration experience and provide better access for their residents, staff, and council … Read more                </div>
+                </div>
+
+            </div>
+        </div>
+        <div className='cards'>
+            <img src={eighteen} className='card-image'/>
+            <div className='cards-texts'>
+                <div className='cards-heading'>
+                The Modern Workplace
+                <div className='cards-para'>
+                Not long ago, meetings took place with everyone in the same room. However, technology has enabled the evolution of how people meet and work, … Read more                </div>
+                </div>
+
+            </div>
+        </div>
+        </div>
+
+        <div className='heading1'>Partnerships</div>
+
+        <div className='partnerImagesContainer'>
+            <img src={p1} className='partnerImages' />
+            <img src={p2} className='partnerImages' />
+            <img src={p3} className='partnerImages' />
+            <img src={p4} className='partnerImages' />
+            <img src={p5} className='partnerImages' />
+            <img src={p6} className='partnerImages' />
+            <img src={p7} className='partnerImages' />
+            <img src={p8} className='partnerImages' />
+            <img src={p9} className='partnerImages' />
+            <img src={p10} className='partnerImages' />
+            <img src={p11} className='partnerImages' />
+            <img src={p12} className='partnerImages' />
+
+        </div>
+            
+        </div>
+    )
+}
+
+export default Partners
