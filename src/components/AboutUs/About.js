1cd0752c3516232c31d9df56f9353f4d35155254
vishaalupot <vishaalsupotofficial@gmail.com>
2024-08-06 14:38:04 +0400

    Spacing in About us

---
+++ src/components/AboutUs/About.js
@@ -0,0 +1,71 @@
+import './About.css';
+
+const About = () => {
+  return (
+    <div className='flex justify-center equal-padding'>
+      <div className='Abtus'>
+      <div className='AbtusText'>
+      <div className='heading'>
+            About Us
+        </div>
+        <div className='AboutText'> 
+        Ford AV is America’s leading audiovisual solutions integrator.  With over 50 years of experience, we are the complete technology provider for the nation’s top enterprise brands for design, installation, and service of professional sound systems, AV systems, video conferencing systems, networks, and security surveillance systems.
+        </div>
+        <button type="submit" class="learn-btn">Learn More</button>
+      </div>
+      <div className='AbtusImage'>
+
+      </div>
+      </div>
+      <div className='ConfImage'>
+      <div class="overlay">
+        <div className='confText'>
+          <div className='confHead'>
+          Solutions for Modern Collaboration
+          </div>
+          <div className='confPara'>
+          Today’s modern workplace runs on Microsoft Teams, Zoom, Cisco Webex, and Google Meet. Join online meetings from the conference room with one touch! Ford AV is a trusted partner of leading software and hardware solutions for the meeting room.
+          </div>
+          <button type="submit" class="talk-btn">Talk To An Expert</button>
+        </div>
+      </div>
+      </div>
+      
+      <div className='VideoDesc'>
+        
+        <div className='Card1'>
+          <div className='mainVideo'>
+
+          </div>
+          <div className='mainText'>
+            <div className='videoHeading'>
+            FAM | First Americans Museum
+            </div>
+            <div className='videoText'>
+            Court Myers, of Waystone LLC, shares how he worked with Ford AV to accomplish AV challenges for the First Americans Museum (FAM) which features Native American stories told from their own perspective.
+            </div>
+            <button type="submit" class="learn-btn">Learn More</button>
+          </div>
+          
+        </div>
+        <div className='Card1'>
+          <div className='mainVideo'>
+
+          </div>
+          <div className='mainText'>
+            <div className='videoHeading'>
+            Indeed
+            </div>
+            <div className='videoText'>
+            With over 13,000 global employees working in over forty-five countries, Indeed uses extensive AV technology to facilitate the workplace community.
+            </div>
+            <button type="submit" class="learn-btn">Learn More</button>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AboutUs from './components/AboutUs/About';
 import Partners from './components/Partner/Partners';
 import Footer from './components/Footer/Footers';
 import reportWebVitals from './reportWebVitals';
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
+    <AboutUs />
     <Partners />
     <Footer />
   </React.StrictMode>
